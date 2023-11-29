a0b87388982f722195f1415a8195ab455ac7e444
pvn1981 <pvn1981@gmail.com>
2023-11-29 11:42:47 +0300

    Added TesterLog example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Examples
-Simple example code
+In this repository I store simple, clear examples
+
+1. TesterLog - Using the log4net library and run one task in Main func
---
+++ TesterLog/TesterLog/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using log4net;
+using log4net.Config;
+
+namespace TesterLog
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static async Task TaskFunc()
+        {
+            int count = 5;
+            while (count > 0)
+            {
+                int delayTimeMS = 1000;
+                await Task.Delay(delayTimeMS);
+                log.InfoFormat("count: {0}", count);
+                log.InfoFormat("Delay ms: {0}", delayTimeMS);
+                count--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Initialize log4net
+            XmlConfigurator.Configure();
+
+            log.Info("Main app started");
+
+            // block while the task completes
+            var task = Task.Run( async () => { await TaskFunc(); } );
+            task.Wait();
+
+            log.Info("Main app ended");
+        }
+    }
+}
