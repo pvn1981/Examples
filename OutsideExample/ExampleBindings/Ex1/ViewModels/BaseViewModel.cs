b3d08ec5ec72ce4fc7d7c2ce186c032df9f190c1
pvn1981 <pvn1981@gmail.com>
2024-01-10 14:18:58 +0300

    Binding ComboBox

---
+++ OutsideExample/ExampleBindings/Ex1/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Ex1.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ OutsideExample/ExampleBindings/Ex1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1.ViewModels
+{
+    namespace Ex1.ViewModels
+    {
+        public class MainWindowViewModel : BaseViewModel
+        {
+            private string _synchronizedText;
+            public string SynchronizedText
+            {
+                get => _synchronizedText;
+                set
+                {
+                    _synchronizedText = value;
+                    OnPropertyChanged(nameof(SynchronizedText));
+                }
+            }
+        }
+    }
+}
