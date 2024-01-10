dd0f66af426daa3d7c3cd8f9f45fece72de3f765
pvn1981 <pvn1981@gmail.com>
2024-01-10 11:49:22 +0300

    Added linq example

---
+++ SimpleCode/ConsoleAppExampleLinq/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleAppExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "bot", "apple", "apricot" };
+
+            int minimalLength = words.Where(w => w.StartsWith("a")).Min(w => w.Length);
+
+            // output: 5
+            Console.WriteLine(minimalLength);
+
+            int[] numbers = { 4, 7, 10 };
+
+            int product = numbers.Aggregate(1, (interim, next) => interim * next);
+
+            // output: 280
+            Console.WriteLine(product);
+        }
+    }
+}
