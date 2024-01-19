a2d2e84841cbca577bbc5d1842ef1f0f3ac7d4a8
pvn1981 <pvn1981@gmail.com>
2024-01-19 15:32:53 +0300

    Ended code for a user interface main window

---
+++ SimpleApp/LoginWithPassword/DesignTime/DesignTimeData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LoginWithPassword.Models;
+
+namespace LoginWithPassword.DesignTime
+{
+    public static class DesignTimeData
+    {
+        internal static MainWindowModel MainWindowModel
+        {
+            get
+            {
+                MainWindowModel model = new MainWindowModel();
+                
+                RakursUser user1 = new RakursUser() { Number = 0, Id = 1, Name = "User1"};
+                RakursUser user2 = new RakursUser() { Number = 1, Id = 2, Name = "User2" };
+                RakursUser user3 = new RakursUser() { Number = 2, Id = 3, Name = "User3" };
+
+                model.Users = new List<RakursUser>();
+                model.Users.Add(user1);
+                model.Users.Add(user2);
+                model.Users.Add(user3);
+
+                return model;
+            }
+        }
+    }
+}
---
+++ SimpleApp/LoginWithPassword/Models/MainWindowModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginWithPassword.Models
+{
+    class RakursUser
+    {
+        public int Number { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class MainWindowModel
+    {
+        public int IndexNumber { get; set; }
+        public List<RakursUser> Users { get; set; }
+    }
+}
