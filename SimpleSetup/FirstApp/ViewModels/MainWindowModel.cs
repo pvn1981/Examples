62ffed42f61fd109544ab6be4a4041a75716c475
Vladimir <pvn1981@gmail.com>
2024-07-02 11:56:42 +0300

    Added application uses InnoSetup as example

---
+++ SimpleSetup/FirstApp/ViewModels/MainWindowModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using CommunityToolkit.Mvvm.Input;
+
+namespace FirstApp
+{
+    class MainWindowModel
+    {
+        public string VersionAppStr
+        {
+            get
+            {
+                Version version = Assembly.GetEntryAssembly().GetName().Version;
+                string versionApp = string.Format("Версия приложения: {0}", version.ToString());
+                return versionApp;
+            }
+        }
+
+        private void ExitApp()
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        public ICommand ExitCommand
+        {
+            get
+            {
+                return new RelayCommand(() => ExitApp());
+            }
+        }
+    }
+}
