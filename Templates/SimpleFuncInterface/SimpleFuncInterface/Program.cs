2e71519421c6f46c10da33ad0e1f047b3687f80d
Vladimir <pvn1981@gmail.com>
2024-07-17 17:46:02 +0300

    Added own first using interface

---
+++ Templates/SimpleFuncInterface/SimpleFuncInterface/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SimpleFuncInterface
+{
+    internal class Program
+    {
+        public interface IEventHandler<T>
+        {
+            void Handle(T e);
+        }
+
+        class Event
+        { }
+
+        class Work
+        { }
+
+
+        class Worker : IEventHandler<Event>, IEventHandler<Work>
+        {
+            public void Handle(Event e)
+            {
+                Console.WriteLine("Worker handle Event");
+            }
+
+            public void Handle(Work e)
+            {
+                Console.WriteLine("Worker handle Work");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+
+            worker.Handle(new Event());
+            worker.Handle(new Work());
+        }
+    }
+}
