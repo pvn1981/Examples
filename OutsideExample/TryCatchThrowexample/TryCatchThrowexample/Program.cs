327c1df164f7ab2e4cada18203efb9f38b59aab3
pvn1981 <pvn1981@gmail.com>
2024-09-19 09:27:44 +0300

    Added TryCatchThrow example

---
+++ OutsideExample/TryCatchThrowexample/TryCatchThrowexample/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchThrowexample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Example 1: re-throw inside of another try block:");
+
+            try
+            {
+                Console.WriteLine("--outer try");
+                try
+                {
+                    Console.WriteLine("----inner try");
+                    throw new Exception();
+                }
+                catch
+                {
+                    Console.WriteLine("----inner catch");
+                    // throw;
+                    throw new Exception();
+                }
+                finally
+                {
+                    Console.WriteLine("----inner finally");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("--outer catch");
+                // swallow
+            }
+            finally
+            {
+                Console.WriteLine("--outer finally");
+            }
+            Console.WriteLine("Huzzah!");
+
+            Console.WriteLine();
+            Console.WriteLine("Example 2: re-throw outside of another try block:");
+            try
+            {
+                Console.WriteLine("--try");
+                throw new Exception();
+            }
+            catch
+            {
+                Console.WriteLine("--catch");
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("--finally");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
