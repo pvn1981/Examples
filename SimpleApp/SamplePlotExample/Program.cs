2a8fde87d1dd3fcce1ba733c27408f77462f4e6e
pvn1981 <pvn1981@gmail.com>
2024-11-19 16:00:15 +0300

    Added simple plot console app (Save image to png)

---
+++ SimpleApp/SamplePlotExample/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SamplePlotExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] dataX = { 1, 2, 3, 4, 5 };
+            double[] dataY = { 1, 4, 9, 16, 25 };
+
+            ScottPlot.Plot myPlot = new();
+            myPlot.Add.Scatter(dataX, dataY);
+
+            myPlot.SavePng("quickstart.png", 400, 300);
+        }
+    }
+}
