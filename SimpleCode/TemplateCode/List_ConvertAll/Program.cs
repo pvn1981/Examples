877d6f707f5c309b5ad4113835fdf848f2d12cea
pvn1981 <pvn1981@gmail.com>
2025-02-04 15:26:43 +0300

    Added ConvertAll using for List<int>

---
+++ SimpleCode/TemplateCode/List_ConvertAll/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace List_ConvertAll;
+class Program
+{
+    static double TakeSquareRoot(int x)
+    {
+        return Math.Sqrt(x);
+    }
+
+    static void Main(string[] args)
+    {
+        List<int> integers = new List<int>();
+        integers.Add(1);
+        integers.Add(2);
+        integers.Add(3);
+        integers.Add(4);
+
+        // Создание и заполнение списка целых чисел
+        // Создание экземпляра делегата
+        Converter<int, double> converter = TakeSquareRoot;
+        List<double> doubles;
+
+        // List<TOutput> ConvertAll<TOutput>(Converter<T, TOutput> converter);
+        doubles = integers.ConvertAll<double>(converter);
+
+        // Вызов обобщенного метода
+        // для преобразования списка
+        int i = 0;
+        foreach (double d in doubles)
+        {
+            Console.WriteLine($"sqrt({integers[i]})={d}");
+            i += 1;
+        }
+    }
+}
---
+++ SimpleCode/dotnet_NET6_create_TemlateCode.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+set SOLUTION_NAME="TemplateCode"
+set CONSOLE_PROJECT_NAME="List_ConvertAll"
+
+dotnet new sln -o %SOLUTION_NAME%
+
+cd .\%SOLUTION_NAME%\
+
+dotnet new console -n %CONSOLE_PROJECT_NAME% --use-program-main --framework net6.0
+
+dotnet sln add %CONSOLE_PROJECT_NAME%/%CONSOLE_PROJECT_NAME%.csproj
+
+rem Check sln
+dotnet sln list   
