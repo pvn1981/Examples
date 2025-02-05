e5975de17996297b2475e49c14b0c001adb26a48
pvn1981 <pvn1981@gmail.com>
2025-02-05 15:52:39 +0300

    Added typeof( example for Enum

---
+++ SimpleCode/TypeofExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TypeofExample;
+
+class Program
+{
+    public enum Cats { Рыжик = 3, Барсик = 5, Мурзик, Васька, Пушок };
+
+
+
+    static void Main(string[] args)
+    {
+        // Перечисляем все элементы перечисления
+        
+        // 1
+        string[] catNames = Enum.GetNames(typeof(Cats));
+
+        //2
+        // string[] catNames = typeof(Cats).GetEnumNames();
+
+        try
+        {
+            foreach (var t in catNames)
+            {
+                Console.WriteLine("t: {0}, iValue: {1}", t, Convert.ToInt32(Enum.Parse<Cats>(t)));
+            }
+        }
+        catch(ArgumentException e)
+        {
+            Console.WriteLine($"e: {e.Message}");
+        }
+    }
+}
---
+++ dotnet_NET6_create_SimpleCode.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+set SOLUTION_NAME="SimpleCode"
+set CONSOLE_PROJECT_NAME="TypeofExample"
+
+dotnet new sln -o %SOLUTION_NAME%
+
+cd .\%SOLUTION_NAME%\
+
+dotnet new console -n %CONSOLE_PROJECT_NAME% --use-program-main --framework net6.0
+
+dotnet sln add %CONSOLE_PROJECT_NAME%/%CONSOLE_PROJECT_NAME%.csproj
+
+rem Check sln
+dotnet sln list   
