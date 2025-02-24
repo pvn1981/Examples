586709b837f89c3c7a38ad047b98ef4415a1636b
pvn1981 <pvn1981@gmail.com>
2025-02-24 14:28:01 +0300

    Added example with Ribbon

---
+++ CommunityToolkit.Mvvm/RibbonAppExample/RibbonAppExample/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace RibbonAppExample
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        public RelayCommand Button1Command { get; }
+
+        public MainWindowViewModel()
+        {
+            Button1Command = new RelayCommand(ExecuteButton1Command);
+        }
+
+        private void ExecuteButton1Command()
+        {
+            // Your command logic here
+            MessageBox.Show("Ribbon button clicked!", "Message Box Title", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
